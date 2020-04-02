1ba91692c7fb032e4928597f8271a77b95d498f3
qhernandez927 <quinnh2791@gmail.com>
2020-04-02 11:38:55 -0500

    Captailization and hash mark added plus padding with zeros to ID

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// DOM objects
 const mainScreen = document.querySelector('.main-screen');
 const pokeName = document.querySelector('.poke-name');
 const pokeId = document.querySelector('.poke-id');
@@ -8,18 +9,44 @@ const pokeTypeTwo = document.querySelector('.poke-type-two');
 const pokeWeight = document.querySelector('.poke-weight');
 const pokeHeight = document.querySelector('.poke-height'); 
 
+//Constants and variables 
+const TYPES = [
+    'normal', 'fighting', 'flying',
+    'poison', 'ground', 'rock',
+    'bug', 'ghost', 'steel',
+    'fire', 'water', 'grass',
+    'electric', 'psyhic', 'ice',
+    'dragon', 'dark', 'fairy',
+];
+
+
+// Functions 
+const capitalize = (str) => str[0].toUpperCase() + str.substr(1);
+
+
+const resetScreen = () => {
+    
+    mainScreen.classList.remove('hide');
+
+    for(const type of TYPES) {
+        mainScreen.classList.remove(type);
+    }
+
+};
+
 const resultFromFetch = fetch ('https://pokeapi.co/api/v2/pokemon/1')
 .then(res =>  res.json())
 .then(data => {
-    console.log(data);
+
+    resetScreen();
    
     const dataTypes = data['types'];
     const dataFirstType = dataTypes[0];
     const dataSecondType = dataTypes[1];
-    pokeTypeOne.textContent = dataFirstType['type']['name'];
+    pokeTypeOne.textContent = capitalize(dataFirstType['type']['name']);
     if (dataSecondType) {
         pokeTypeTwo.classList.remove('hide');
-       pokeTypeTwo.textContent = dataSecondType['type']['name']; 
+       pokeTypeTwo.textContent = capitalize (dataSecondType['type']['name']); 
     } else {
         pokeTypeTwo.classList.add('hide');
         pokeTypeTwo.textContent = '';
@@ -27,9 +54,10 @@ const resultFromFetch = fetch ('https://pokeapi.co/api/v2/pokemon/1')
    
     mainScreen.classList.add(dataFirstType['type']['name']);
     mainScreen.classList.remove('hide');
-    
-    pokeName.textContent = data['name'];
-    pokeId.textContent = data['id'];
+
+
+    pokeName.textContent = capitalize (data['name']);
+    pokeId.textContent = '#' + data['id'].toString().padStart(3,'0');
     pokeWeight.textContent = data['weight'];
     pokeHeight.textContent = data['height'];
 
